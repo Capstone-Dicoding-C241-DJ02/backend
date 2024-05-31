419a19cd2a313638308f8de045f1ec6f39357f71
alansugito <alansugito75@gmail.com>
2024-05-31 12:56:19 +0700

    remove database batch insert code

--- src/apps/prismaClient.js
+++ src/apps/prismaClient.js
@@ -1,27 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import fs from "fs";
+
 
 const prismaClient = new PrismaClient({
   errorFormat: "pretty",
   log: ["error", "warn"],
 });
 
-fs.readFile("../../jobs-dataset.json", (err, data) => {
-  if (err) console.log(err);
-
-  const json = JSON.parse(data.toString());
-
-  prismaClient.job
-    .createMany({
-      data: json.map((d) => ({
-        title: d.title,
-        desc: d.description,
-        city: d.city,
-        logo: d.Logo,
-        business_sector: d.business_sector,
-      })),
-    })
-    .then(() => console.log("ok"));
-});
 
 export default prismaClient;
